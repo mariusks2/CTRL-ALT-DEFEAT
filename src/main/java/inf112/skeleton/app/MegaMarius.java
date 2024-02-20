cf688ce35b54c3ef1aa940e014251e3e01df8b21
ollav12 <90145593+ollav12@users.noreply.github.com>
2024-02-20 11:20:30 +0100

    Endre kode plassering og lagt til litt kode

--- src/main/java/inf112/skeleton/app/MegaMarius.java
+++ src/main/java/inf112/skeleton/app/MegaMarius.java
@@ -6,25 +6,21 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import inf112.screens.PlayScreen;
+
 public class MegaMarius extends Game {
-    private SpriteBatch batch;
-    private Texture img;
+    public SpriteBatch batch;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        img = new Texture(Gdx.files.internal("MARIUS.png"));
+        setScreen(new PlayScreen(this));
     }
 
     @Override
     public void render() {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        super.render();
 
-        // Draw calls should be wrapped in batch.begin() ... batch.end()
-        batch.begin();
-        batch.draw(img, 0, 0);
-        batch.end();
     }
 
 }

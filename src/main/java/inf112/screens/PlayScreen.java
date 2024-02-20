4fb491ce3150fa90a8eadba59503d76da64fe712
ollav12 <90145593+ollav12@users.noreply.github.com>
2024-02-20 11:34:25 +0100

    Testet litt ulike måter å vise bilde skjermen

--- src/main/java/inf112/screens/PlayScreen.java
+++ src/main/java/inf112/screens/PlayScreen.java
@@ -3,7 +3,12 @@ package inf112.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 import inf112.skeleton.app.MegaMarius;
 
@@ -11,10 +16,14 @@ public class PlayScreen implements Screen {
 
     private MegaMarius game;
     Texture texture;
+    private OrthographicCamera gamecam;
+    private Viewport gamePort;
 
     public PlayScreen(MegaMarius game) {
         this.game = game;
         texture = new Texture(Gdx.files.internal("MARIUS.png"));
+        gamecam = new OrthographicCamera();
+        gamePort = new FitViewport(800, 480, gamecam);
     }
 
     @Override
@@ -26,6 +35,7 @@ public class PlayScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.setProjectionMatrix(gamecam.combined);
 
         game.batch.begin();
         game.batch.draw(texture, 0, 0);
@@ -34,6 +44,7 @@ public class PlayScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
+        gamePort.update(width, height);
 
     }
 

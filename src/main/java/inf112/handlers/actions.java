aa4bd18362aed76040397cd2757fa3530775320b
Chrissle <chrisslettebo@gmail.com>
2024-02-21 12:42:34 +0100

    bevegelig marius bilde

---
+++ src/main/java/inf112/handlers/actions.java
@@ -0,0 +1,12 @@
+package inf112.handlers;
+
+public enum actions {
+    JUMP,
+    MOVE_LEFT,
+    MOVE_RIGHT,
+    CROUCH,
+    START,
+    PAUSE,
+    NO_ACTION,
+    COMPLETED
+}
--- src/main/java/inf112/handlers/inputHandler.java
+++ src/main/java/inf112/handlers/inputHandler.java
@@ -10,66 +10,58 @@ import inf112.view.mariusView;
 
 public class inputHandler implements KeyListener, MouseListener{
 
-    void InputManager(MegaMarius megaMarius, mariusView view) {
-        
+    MegaMarius megaMarius;
+
+    public inputHandler(MegaMarius megaMarius) {
+            this.megaMarius=megaMarius;
         }
 
     @Override
     public void keyPressed(KeyEvent event) {
         int keyCode = event.getKeyCode();
-        GameStatus status = engine.getGameStatus();
-        ButtonAction currentAction = ButtonAction.NO_ACTION;
 
         if (keyCode == KeyEvent.VK_UP) {
+            /* 
             if(status == GameStatus.START_SCREEN || status == GameStatus.MAP_SELECTION)
                 currentAction = ButtonAction.GO_UP;
             else
                 currentAction = ButtonAction.JUMP;
+                */
+
         }
         else if(keyCode == KeyEvent.VK_DOWN){
-            if(status == GameStatus.START_SCREEN || status == GameStatus.MAP_SELECTION)
-                currentAction = ButtonAction.GO_DOWN;
+            //if(status == GameStatus.START_SCREEN || status == GameStatus.MAP_SELECTION)
+            
         }
         else if (keyCode == KeyEvent.VK_RIGHT) {
-            currentAction = ButtonAction.M_RIGHT;
+            System.out.println("hei");
+            
         }
         else if (keyCode == KeyEvent.VK_LEFT) {
-            currentAction = ButtonAction.M_LEFT;
+            
         }
         else if (keyCode == KeyEvent.VK_ENTER) {
-            currentAction = ButtonAction.SELECT;
+            //
         }
         else if (keyCode == KeyEvent.VK_ESCAPE) {
-            if(status == GameStatus.RUNNING || status == GameStatus.PAUSED )
+            /*if(status == GameStatus.RUNNING || status == GameStatus.PAUSED )
                 currentAction = ButtonAction.PAUSE_RESUME;
             else
-                currentAction = ButtonAction.GO_TO_START_SCREEN;
+                currentAction = ButtonAction.GO_TO_START_SCREEN;*/
 
         }
         else if (keyCode == KeyEvent.VK_SPACE){
-            currentAction = ButtonAction.FIRE;
+            //
         }
-
-
-        notifyInput(currentAction);
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
-        if(engine.getGameStatus() == GameStatus.MAP_SELECTION){
-            engine.selectMapViaMouse();
-        }
+        
     }
 
     @Override
     public void keyReleased(KeyEvent event) {
-        if(event.getKeyCode() == KeyEvent.VK_RIGHT || event.getKeyCode() == KeyEvent.VK_LEFT)
-            notifyInput(ButtonAction.ACTION_COMPLETED);
-    }
-
-    private void notifyInput(ButtonAction action) {
-        if(action != ButtonAction.NO_ACTION)
-            engine.receiveInput(action);
     }
 
     @Override

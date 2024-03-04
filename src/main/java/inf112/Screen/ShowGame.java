4f6dd482de191bd59201478aeee7d96451a67523
Marius Soma <mariusk.soma@gmail.com>
2024-03-04 13:38:17 +0100

    map now shows

---
+++ src/main/java/inf112/Screen/ShowGame.java
@@ -0,0 +1,90 @@
+package inf112.Screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+import inf112.skeleton.app.MegaMarius;
+
+public class ShowGame implements Screen{
+    private MegaMarius game;
+    private OrthographicCamera gamecam;
+    private Viewport gamePort;
+
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+    
+
+    public ShowGame(MegaMarius game){
+        this.game = game;
+        gamecam = new OrthographicCamera();
+        gamePort = new FitViewport(MegaMarius.M_Width, MegaMarius.M_Height, gamecam);
+
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("mario1.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        gamecam.position.set(gamePort.getWorldWidth()/2, gamePort.getWorldHeight()/2, 0);
+
+    }
+
+    public void update(float dt){
+        handleInput(dt);
+
+        gamecam.update();
+        renderer.setView(gamecam);
+    }
+
+    @Override
+    public void render(float delta) {
+        update(delta);
+        Gdx.gl.glClearColor(0,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); //clears screen
+
+        renderer.render();
+    }
+
+    private void handleInput(float dt) {
+        if (Gdx.input.isTouched()) {
+            gamecam.position.x += 100*dt;
+        }
+    }
+
+
+    @Override
+    public void show() {
+        
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        gamePort.update(width, height);
+    }
+
+    @Override
+    public void pause() {
+        
+    }
+
+    @Override
+    public void resume() {
+        
+    }
+
+    @Override
+    public void hide() {
+        
+    }
+
+    @Override
+    public void dispose() {
+        
+    }
+    
+}

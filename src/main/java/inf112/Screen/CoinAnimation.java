4530f7ad1c69ca430b9aee90ea96ebadb8b7c557
Marius Soma <mariusk.soma@gmail.com>
2024-04-11 14:34:46 +0200

    coins fix

--- src/main/java/inf112/Screen/CoinAnimation.java
+++ src/main/java/inf112/Screen/CoinAnimation.java
@@ -1,20 +1,14 @@
 package inf112.Screen;
-
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.CircleShape;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
 
 import inf112.Screens.ShowGame;
 import inf112.skeleton.app.Marius;
-import inf112.skeleton.app.MegaMarius;
 
 public class CoinAnimation extends Item{
     
     public CoinAnimation(ShowGame screen, float x, float y){
         super(screen, x, y);
-        setRegion(screen.getAtlas().findRegion("pepsi"));
+        setRegion(screen.getAtlas().findRegion("coin"));
     } 
 
     @Override

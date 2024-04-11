ebea8a6ed4b36e9af1fc91098f4bb9c4197c625a
Marius Soma <mariusk.soma@gmail.com>
2024-04-11 14:01:53 +0200

    added so coin shows at headhit

--- src/main/java/inf112/Screen/Coin.java
+++ src/main/java/inf112/Screen/Coin.java
@@ -1,5 +1,9 @@
 package inf112.Screen;
+import java.util.concurrent.TimeUnit;
+
 import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
 import com.badlogic.gdx.maps.tiled.TiledMapTileSet;
 import com.badlogic.gdx.math.Vector2;
 
@@ -11,6 +15,7 @@ import inf112.skeleton.app.MegaMarius;
 public class Coin extends InteractiveTileObj{
     private static TiledMapTileSet tileSet;
     private final int BLANK_COIN = 4;
+    private final int COIN = 58;
 
     public Coin(ShowGame screen, MapObject object){
         super(screen, object);
@@ -28,9 +33,17 @@ public class Coin extends InteractiveTileObj{
                 screen.spawnItems(new ItemDef(new Vector2(body.getPosition().x, body.getPosition().y + 16/MegaMarius.PPM), Pepsi.class));
             }
             else if(object.getProperties().containsKey("coin")){
-                screen.spawnItems(new ItemDef(new Vector2(body.getPosition().x, body.getPosition().y + 16/MegaMarius.PPM), CoinAnimation.class));
+                getCellA().setTile(tileSet.getTile(COIN)); //
+                getCellA().setTile(null); // Set tile to null (removes the block from map)
+                Display.updateScore(200); // Update score
             }
              
         }
+    }
+    public TiledMapTileLayer.Cell getCellA(){
+        TiledMapTileLayer layer = (TiledMapTileLayer) map.getLayers().get(8);
+        layer.setVisible(true);
+        return layer.getCell((int)(body.getPosition().x * MegaMarius.PPM / 16),
+                (int)((body.getPosition().y) * MegaMarius.PPM / 16+1));
     }
 }

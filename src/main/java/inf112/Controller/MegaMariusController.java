acad5716676cc3fab4bc4e0c98d0624ab5364415
Marius Soma <mariusk.soma@gmail.com>
2024-05-02 19:26:52 +0200

    added WAD to playermovement

--- src/main/java/inf112/Controller/MegaMariusController.java
+++ src/main/java/inf112/Controller/MegaMariusController.java
@@ -18,11 +18,11 @@ public class MegaMariusController implements MegaMariusControllable {
     public void handlePlayerMovement() {
          //control our player using immediate impulses
         if (player.currentState != Marius.State.DEAD) {
-            if (Gdx.input.isKeyJustPressed(Input.Keys.UP))
+            if (Gdx.input.isKeyJustPressed(Input.Keys.UP) || (Gdx.input.isKeyJustPressed(Input.Keys.W)))
                 player.jump();
-            if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && player.b2body.getLinearVelocity().x <= 2)
+            if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) || (Gdx.input.isKeyPressed(Input.Keys.D) && player.b2body.getLinearVelocity().x <= 2))
                 player.b2body.applyLinearImpulse(new Vector2(0.05f, 0), player.b2body.getWorldCenter(), true);
-            if (Gdx.input.isKeyPressed(Input.Keys.LEFT) && player.b2body.getLinearVelocity().x >= -2)
+            if (Gdx.input.isKeyPressed(Input.Keys.LEFT) || (Gdx.input.isKeyPressed(Input.Keys.A) && player.b2body.getLinearVelocity().x >= -2))
                 player.b2body.applyLinearImpulse(new Vector2(-0.05f, 0), player.b2body.getWorldCenter(), true);
         } 
     } 

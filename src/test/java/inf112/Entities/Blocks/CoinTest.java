14e0dbe7f42fa552c30c6725f07fac059a7b737a
Chrissle <chrisslettebo@gmail.com>
2024-05-02 18:53:24 +0200

    more cleaning tests

--- src/test/java/inf112/Entities/Blocks/CoinTest.java
+++ src/test/java/inf112/Entities/Blocks/CoinTest.java
@@ -1,9 +1,7 @@
 package inf112.Entities.Blocks;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeAll;
@@ -14,13 +12,11 @@ import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3NativesLoader;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
@@ -57,8 +53,6 @@ public class CoinTest {
         HeadlessApplicationConfiguration config = new HeadlessApplicationConfiguration();
 		ApplicationListener listener = new ApplicationAdapter() {
 		};
-        // Provide a stub for glGenTexture() method to avoid further issues
-        // Provide a stub for glGenTexture() method to avoid further issues
         gl = mock(GL20.class);
         Application app = mock(Application.class);
         //Mock Gdx
@@ -66,6 +60,7 @@ public class CoinTest {
         Gdx.gl = gl;
         
         new HeadlessApplication(listener, config);
+        //make instances or mocks of classes we need to test
         ShowGame cScreen = mock(ShowGame.class);
         World world = new World(new Vector2(0, -10), true);
         display = new Display(mock(SpriteBatch.class));

7b88a9124f9f833a284a049582bd94c7a9f06103
Chrissle <chrisslettebo@gmail.com>
2024-05-03 13:26:57 +0200

    more tests, test controller and gameworldmanager

---
+++ src/test/java/inf112/Controller/MegaMariusControllerTest.java
@@ -0,0 +1,132 @@
+package inf112.Controller;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3NativesLoader;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2D;
+
+import inf112.Model.World.GameWorldManager;
+import inf112.Model.app.Marius;
+import inf112.Model.app.MegaMarius;
+import inf112.View.Scenes.Display;
+import inf112.View.Screens.ShowGame;
+
+public class MegaMariusControllerTest {
+    
+	Marius marius;
+    TmxMapLoader mapLoader;
+    String fileName = "MapAndTileset/level1.tmx";
+    TiledMap map;
+    GL20 gl;
+    Display display;
+    TextureAtlas textureAtlas;
+	GameWorldManager gameWorldManager;
+    MegaMariusController megaMariusController;
+
+    @BeforeAll
+	static void setUpBeforeAll() {
+        
+    }
+    @BeforeEach
+	void setUpBeforeEach() {
+        Lwjgl3NativesLoader.load();
+
+        // Initialize Box2D
+        Box2D.init();
+        HeadlessApplicationConfiguration config = new HeadlessApplicationConfiguration();
+		ApplicationListener listener = new ApplicationAdapter() {
+		};
+		MegaMarius megaMarius = new MegaMarius();
+        gl = mock(GL20.class);
+        Application app = mock(Application.class);
+        //Mock Gdx
+        Gdx.app = app;
+        Gdx.gl = gl;
+		Gdx.gl20 = gl;
+        
+        new HeadlessApplication(megaMarius, config);
+		megaMarius.setSpriteBatch(mock(SpriteBatch.class));
+        ShowGame cScreen = mock(ShowGame.class);
+        display = new Display(mock(SpriteBatch.class));
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load(fileName);
+		when(cScreen.getDisplay()).thenReturn(display);
+        textureAtlas = new TextureAtlas("Characters/MegaMariusCharacters.pack");
+		gameWorldManager = new GameWorldManager(fileName, textureAtlas);
+        when(cScreen.getAtlas()).thenReturn(textureAtlas);
+        marius = new Marius(cScreen, gameWorldManager.getWorld());
+        megaMariusController = new MegaMariusController(marius);
+	}
+
+    @Test
+    void handleInputTest(){
+        Input input = mock(Input.class);
+        when(input.isKeyJustPressed(Input.Keys.UP)).thenReturn(true);
+        Gdx.input = input;
+        megaMariusController.handlePlayerMovement();
+    }
+
+    @Test
+    void handleInputTest2(){
+        Input input = mock(Input.class);
+        marius.b2body.setLinearVelocity(new Vector2(0,0));
+        when(input.isKeyJustPressed(Input.Keys.RIGHT)).thenReturn(true);
+        Gdx.input = input;
+        megaMariusController.handlePlayerMovement();
+    }
+
+    @Test
+    void handleInputTest3(){
+        Input input = mock(Input.class);
+        marius.b2body.setLinearVelocity(new Vector2(0,0));
+        when(input.isKeyJustPressed(Input.Keys.LEFT)).thenReturn(true);
+        Gdx.input = input;
+        megaMariusController.handlePlayerMovement();
+    }
+
+    @Test
+    void handleInputTest4(){
+        Input input = mock(Input.class);
+        when(input.isKeyJustPressed(Input.Keys.W)).thenReturn(true);
+        Gdx.input = input;
+        megaMariusController.handlePlayerMovement();
+    }
+
+    @Test
+    void handleInputTest5(){
+        Input input = mock(Input.class);
+        marius.b2body.setLinearVelocity(new Vector2(3,0));
+        when(input.isKeyJustPressed(Input.Keys.D)).thenReturn(true);
+        marius.update(0);
+        Gdx.input = input;
+        megaMariusController.handlePlayerMovement();
+    }
+
+    @Test
+    void handleInputTest6(){
+        Input input = mock(Input.class);
+        marius.b2body.setLinearVelocity(new Vector2(3,0));
+        when(input.isKeyJustPressed(Input.Keys.A)).thenReturn(true);
+        marius.update(0);
+        Gdx.input = input;
+        megaMariusController.handlePlayerMovement();
+    }
+}
